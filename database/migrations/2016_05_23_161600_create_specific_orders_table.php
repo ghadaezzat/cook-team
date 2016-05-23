beca41d14a91badd0ae3df3adb2b8933d7cd8e63
mariamAhmedos <mariam2500ahmed@gmail.com>
2016-05-23 18:32:56 +0200

    creating database v1

--- database/migrations/2016_05_23_161600_create_specific_orders_table.php
+++ database/migrations/2016_05_23_161600_create_specific_orders_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateSpecificOrdersTable extends Migration
 {
     /**
      * Run the migrations.
@@ -12,15 +12,13 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('specific_orders', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
-            $table->string('email')->unique();
-            $table->string('phone')->unique();
-            $table->string('password');
-            $table->string('address');
-            $table->boolean('usertype');
-            $table->rememberToken();
+            $table->float('price');
+            $table->float('time');
+            $table->integer('quantity');
+            $table->string('description');
             $table->timestamps();
         });
     }
@@ -32,6 +30,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::drop('users');
+        Schema::drop('specific_orders');
     }
 }
